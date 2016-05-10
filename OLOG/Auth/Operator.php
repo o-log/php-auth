b501756d6e2c9024b523555a25afbeab7f54a6fe
ologinov <olognv@gmail.com>
2016-05-10 19:39:07 +0300

    1

--- OLOG/Auth/Operator.php
+++ OLOG/Auth/Operator.php
@@ -5,6 +5,10 @@ namespace OLOG\Auth;
 class Operator
 {
     static public function currentOperatorHasAnyOfPermissions($requested_permissions_arr){
+        if (!isset($_COOKIE['php_auth'])){
+            return false;
+        }
+
         return true; // TODO: todo
     }
 }
