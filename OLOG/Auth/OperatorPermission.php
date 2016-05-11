73f54e09517f03cd28fabdaf8a24c151dc915b88
ologinov <olognv@gmail.com>
2016-05-11 23:43:07 +0300

    1

---
+++ OLOG/Auth/OperatorPermission.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace OLOG\Auth;
+
+class OperatorPermission implements
+    \OLOG\Model\InterfaceFactory,
+    \OLOG\Model\InterfaceLoad,
+    \OLOG\Model\InterfaceSave,
+    \OLOG\Model\InterfaceDelete
+{
+    use \OLOG\Model\FactoryTrait;
+    use \OLOG\Model\ActiveRecord;
+    use \OLOG\Model\ProtectProperties;
+
+    const DB_ID = 'db_phpauth';
+    const DB_TABLE_NAME = 'olog_auth_operatorpermission';
+
+    protected $created_at_ts; // initialized by constructor
+    protected $operator_id;
+    protected $permission_id;
+    protected $id;
+
+    public function getPermissionId(){
+        return $this->permission_id;
+    }
+
+    public function setPermissionId($value){
+        $this->permission_id = $value;
+    }
+
+
+    static public function getIdsArrForOperatorIdByCreatedAtDesc($value){
+        $ids_arr = \OLOG\DB\DBWrapper::readColumn(
+            self::DB_ID,
+            'select id from ' . self::DB_TABLE_NAME . ' where operator_id = ? order by created_at_ts desc',
+            array($value)
+        );
+        return $ids_arr;
+    }
+
+    public function getOperatorId(){
+        return $this->operator_id;
+    }
+
+    public function setOperatorId($value){
+        $this->operator_id = $value;
+    }
+
+
+    static public function getAllIdsArrByCreatedAtDesc(){
+        $ids_arr = \OLOG\DB\DBWrapper::readColumn(
+            self::DB_ID,
+            'select id from ' . self::DB_TABLE_NAME . ' order by created_at_ts desc'
+        );
+        return $ids_arr;
+    }
+
+    public function __construct(){
+        $this->created_at_ts = time();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAtTs()
+    {
+        return $this->created_at_ts;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setCreatedAtTs($title)
+    {
+        $this->created_at_ts = $title;
+    }
+}
