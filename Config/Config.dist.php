07620b5079dc875384f45ff3d724675592b42619
ologinov <olognv@gmail.com>
2016-05-12 22:51:04 +0300

    1

--- Config/Config.dist.php
+++ Config/Config.dist.php
@@ -6,22 +6,14 @@ use OLOG\Auth\Constants;
 
 class Config
 {
-    const DB_NAME_PHPAUTHDEMO = 'db_phpauthdemo';
-
     public static function get()
     {
         $conf['cache_lifetime'] = 60;
         $conf['return_false_if_no_route'] = true; // for local php server
         $conf['db'] = array(
-            self::DB_NAME_PHPAUTHDEMO => array(
-                'host' => 'localhost',
-                'db_name' => 'db_phpauthdemo',
-                'user' => 'root',
-                'pass' => '1'
-            ),
             Constants::DB_NAME_PHPAUTH => array(
                 'host' => 'localhost',
-                'db_name' => 'db_phpauth',
+                'db_name' => 'db_phpauthdemo',
                 'user' => 'root',
                 'pass' => '1'
             )
