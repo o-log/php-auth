a943b34012af891a09973453fb167f5cb73a10b4
ologinov <olognv@gmail.com>
2016-08-18 18:34:28 +0300

    1

---
+++ OLOG/Auth/PermissionToUser.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace OLOG\Auth;
+
+class PermissionToUser implements
+    \OLOG\Model\InterfaceFactory,
+    \OLOG\Model\InterfaceLoad,
+    \OLOG\Model\InterfaceSave,
+    \OLOG\Model\InterfaceDelete
+{
+    use \OLOG\Model\FactoryTrait;
+    use \OLOG\Model\ActiveRecordTrait;
+    use \OLOG\Model\ProtectPropertiesTrait;
+
+    const DB_ID = 'db_phpauth';
+    const DB_TABLE_NAME = 'olog_auth_permissiontouser';
+
+    protected $created_at_ts; // initialized by constructor
+    protected $user_id;
+    protected $permission_id;
+    protected $id;
+
+    static public function getIdsArrForPermissionIdByCreatedAtDesc($value, $offset = 0, $page_size = 30){
+        if (is_null($value)) {
+            return \OLOG\DB\DBWrapper::readColumn(
+                self::DB_ID,
+                'select id from ' . self::DB_TABLE_NAME . ' where permission_id is null order by created_at_ts desc limit ' . intval($page_size) . ' offset ' . intval($offset)
+            );
+        } else {
+            return \OLOG\DB\DBWrapper::readColumn(
+                self::DB_ID,
+                'select id from ' . self::DB_TABLE_NAME . ' where permission_id = ? order by created_at_ts desc limit ' . intval($page_size) . ' offset ' . intval($offset),
+                array($value)
+            );
+        }
+    }
+
+
+    public function getPermissionId(){
+        return $this->permission_id;
+    }
+
+    public function setPermissionId($value){
+        $this->permission_id = $value;
+    }
+
+
+
+    static public function getIdsArrForUserIdByCreatedAtDesc($value, $offset = 0, $page_size = 30){
+        if (is_null($value)) {
+            return \OLOG\DB\DBWrapper::readColumn(
+                self::DB_ID,
+                'select id from ' . self::DB_TABLE_NAME . ' where user_id is null order by created_at_ts desc limit ' . intval($page_size) . ' offset ' . intval($offset)
+            );
+        } else {
+            return \OLOG\DB\DBWrapper::readColumn(
+                self::DB_ID,
+                'select id from ' . self::DB_TABLE_NAME . ' where user_id = ? order by created_at_ts desc limit ' . intval($page_size) . ' offset ' . intval($offset),
+                array($value)
+            );
+        }
+    }
+
+
+    public function getUserId(){
+        return $this->user_id;
+    }
+
+    public function setUserId($value){
+        $this->user_id = $value;
+    }
+
+
+
+    static public function getAllIdsArrByCreatedAtDesc($offset = 0, $page_size = 30){
+        $ids_arr = \OLOG\DB\DBWrapper::readColumn(
+            self::DB_ID,
+            'select id from ' . self::DB_TABLE_NAME . ' order by created_at_ts desc limit ' . intval($page_size) . ' offset ' . intval($offset)
+        );
+        return $ids_arr;
+    }
+
+    public function __construct(){
+        $this->created_at_ts = time();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAtTs()
+    {
+        return $this->created_at_ts;
+    }
+
+    /**
+     * @param string $timestamp
+     */
+    public function setCreatedAtTs($timestamp)
+    {
+        $this->created_at_ts = $timestamp;
+    }
+}
