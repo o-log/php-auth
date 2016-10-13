727b628b6feb0733cd2b0c8a3f5a8c0b94b7c317
ologinov <olognv@gmail.com>
2016-10-13 18:36:43 +0300

    1

--- OLOG/Auth/Admin/AuthAdminMenu.php
+++ OLOG/Auth/Admin/AuthAdminMenu.php
@@ -2,6 +2,7 @@
 
 namespace OLOG\Auth\Admin;
 
+use OLOG\Auth\Operator;
 use OLOG\Auth\Permissions;
 use OLOG\Layouts\InterfaceMenu;
 use OLOG\Layouts\MenuItem;
@@ -10,14 +11,20 @@ class AuthAdminMenu implements InterfaceMenu
 {
     static public function menuArr()
     {
-        return [
-            new MenuItem('Авторизация', '', [
-                new MenuItem(UsersListAction::pageTitle(), UsersListAction::getUrl(), NULL, 'glyphicon glyphicon-user', [Permissions::PERMISSION_PHPAUTH_MANAGE_USERS]),
-                new MenuItem(OperatorsListAction::pageTitle(), OperatorsListAction::getUrl(), NULL, 'glyphicon glyphicon-eye-open', [Permissions::PERMISSION_PHPAUTH_MANAGE_OPERATORS]),
-                new MenuItem(PermissionsListAction::pageTitle(), PermissionsListAction::getUrl(), NULL, 'glyphicon glyphicon-check', [Permissions::PERMISSION_PHPAUTH_MANAGE_USERS_PERMISSIONS]),
-                new MenuItem(GroupsListAction::pageTitle(), GroupsListAction::getUrl(), NULL, 'glyphicon glyphicon-check', [Permissions::PERMISSION_PHPAUTH_MANAGE_GROUPS]),
-            ], 'glyphicon glyphicon-log-in', [Permissions::PERMISSION_PHPAUTH_MANAGE_USERS, Permissions::PERMISSION_PHPAUTH_MANAGE_GROUPS, Permissions::PERMISSION_PHPAUTH_MANAGE_OPERATORS])
-        ];
+        $menu_arr = [];
+
+        if (Operator::currentOperatorHasAnyOfPermissions([Permissions::PERMISSION_PHPAUTH_MANAGE_USERS, Permissions::PERMISSION_PHPAUTH_MANAGE_GROUPS, Permissions::PERMISSION_PHPAUTH_MANAGE_OPERATORS])) {
+            $menu_arr = [
+                new MenuItem('Авторизация', '', [
+                    new MenuItem(UsersListAction::pageTitle(), UsersListAction::getUrl(), NULL, 'glyphicon glyphicon-user'),
+                    new MenuItem(OperatorsListAction::pageTitle(), OperatorsListAction::getUrl(), NULL, 'glyphicon glyphicon-eye-open'),
+                    new MenuItem(PermissionsListAction::pageTitle(), PermissionsListAction::getUrl(), NULL, 'glyphicon glyphicon-check'),
+                    new MenuItem(GroupsListAction::pageTitle(), GroupsListAction::getUrl(), NULL, 'glyphicon glyphicon-check'),
+                ], 'glyphicon glyphicon-log-in')
+            ];
+        }
+
+        return $menu_arr;
     }
 
 }
