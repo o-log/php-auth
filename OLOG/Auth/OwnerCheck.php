64c06929d23771ad9241d630232cc15396f15cd9
mkozlov1 <650465@gmail.com>
2016-10-18 15:51:53 +0300

    1

--- OLOG/Auth/OwnerCheck.php
+++ OLOG/Auth/OwnerCheck.php
@@ -8,36 +8,46 @@ class OwnerCheck
      * @param $obj InterfaceOwner
      * @return bool
      */
-    static public function currentUserOwnsObj($obj){
+    static public function currentUserOwnsObj($obj)
+    {
         $current_user_id = Auth::currentUserId();
+        return self::userOwnsObj($current_user_id, $obj);
+    }
 
-        if (!$current_user_id){
+    /**
+     * @param $user_id
+     * @param $obj InterfaceOwner
+     * @return bool
+     */
+    static public function userOwnsObj($user_id, $obj)
+    {
+        if (!$user_id) {
             return false;
         }
 
-        if (!($obj instanceof InterfaceOwner)){
+        if (!($obj instanceof InterfaceOwner)) {
             return false;
         }
 
-        $current_user_obj = User::factory($current_user_id);
-        if ($current_user_obj->getHasFullAccess()){
+        $current_user_obj = User::factory($user_id);
+        if ($current_user_obj->getHasFullAccess()) {
             return true;
         }
 
-        $current_user_usertogroup_ids_arr = UserToGroup::getIdsArrForUserIdByCreatedAtDesc($current_user_id);
+        $current_user_usertogroup_ids_arr = UserToGroup::getIdsArrForUserIdByCreatedAtDesc($user_id);
         $current_user_groups_ids_arr = [];
 
-        foreach ($current_user_usertogroup_ids_arr as $usertogroup_id){
+        foreach ($current_user_usertogroup_ids_arr as $usertogroup_id) {
             $usertogroup_obj = UserToGroup::factory($usertogroup_id);
             $current_user_groups_ids_arr[] = $usertogroup_obj->getGroupId();
         }
 
-        if ($obj->getOwnerUserId() == $current_user_id){
+        if ($obj->getOwnerUserId() == $user_id) {
             return true;
         }
 
         $obj_owner_group_id = $obj->getOwnerGroupId();
-        if (in_array($obj_owner_group_id, $current_user_groups_ids_arr)){
+        if (in_array($obj_owner_group_id, $current_user_groups_ids_arr)) {
             return true;
         }
 
