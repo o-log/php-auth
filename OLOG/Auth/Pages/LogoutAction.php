ba66f8b348baeb4161c7002dcda18baedc47a773
mkozlov1 <650465@gmail.com>
2016-12-13 15:20:28 +0300

    1

--- OLOG/Auth/Pages/LogoutAction.php
+++ OLOG/Auth/Pages/LogoutAction.php
@@ -8,22 +8,16 @@ use OLOG\Sanitize;
 
 class LogoutAction
 {
-    static public function getUrl(){
+    static public function getUrl()
+    {
         return '/auth/logout';
     }
 
-    public function action(){
+    public function action()
+    {
         Auth::logout();
 
-        // remove extra cookies
-        if (!empty(AuthConfig::getExtraCookiesArr())){
-            $extra_cookies_arr = AuthConfig::getExtraCookiesArr();
-
-            foreach ($extra_cookies_arr as $cookie_name => $cookie_value){
-                //setcookie($cookie_name, $cookie_value, time() + Auth::SESSION_LIFETIME_SECONDS, '/', Auth::sessionCookieDomain());
-                setcookie($cookie_name, "", 1000, '/', Auth::sessionCookieDomain(), false, true);
-            }
-        }
+        self::unsetExtraCookies();
 
         $redirect = '/';
         if (isset($_GET['destination'])) {
@@ -31,5 +25,32 @@ class LogoutAction
         }
 
         \OLOG\Redirects::redirect($redirect);
+    }
+
+    public static function unsetExtraCookies()
+    {
+        $extra_cookies_arr = AuthConfig::getExtraCookiesArr();
+        if (empty($extra_cookies_arr)) {
+            return;
+        }
+
+        $extra_cookies_arr = AuthConfig::getExtraCookiesArr();
+
+        foreach ($extra_cookies_arr as $cookie_name => $cookie_value) {
+            if ($cookie_value instanceof \OLOG\Auth\ExtraCookie) {
+                $extra_cookie_obj = $cookie_value;
+                setcookie(
+                    $extra_cookie_obj->getCookieName(),
+                    '',
+                    1000,
+                    '/',
+                    Auth::sessionCookieDomain(),
+                    $extra_cookie_obj->isSecure(),
+                    $extra_cookie_obj->isHttpOnly()
+                );
+            } else {
+                setcookie($cookie_name, '', 1000, '/', Auth::sessionCookieDomain(), false, true);
+            }
+        }
     }
 }
